c556a6a0efc1571f927b0bfd829e8cbd9005e09d
Padmajaa26 <143872286+Padmajaa26@users.noreply.github.com>
2024-09-05 22:35:59 +0530

    Delete Placement1 directory

--- Placement1/src/main/java/com/example/Placement/repository/PlacementRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Placement.repository;
-
-import com.example.Placement.model.Placement;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlacementRepository extends JpaRepository<Placement, Long> {
-}
